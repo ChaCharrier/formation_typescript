5907cc8376b808368d945aa84a0f93b82317a3d6
Charlotte Charrier <charlotte.charrier@maif.fr>
2026-01-19 11:33:58 +0100

    exercice 4

--- 04_arrow.js
+++ 04_arrow.js
@@ -1,23 +1,28 @@
-
 /**
  * Déclarer une fonction fléchée qui renvoi l'argument + 1
  * Le return est explicite
- * 
- * contrainte: 
+ *
+ * contrainte:
  *   - le mot clée "function" est interdis
  */
 
-let arrow1 = () => {}
+let arrow1 = (a) => {
+  return a + 1;
+};
 
 /**
  * Déclarer une fonction fléchée qui renvoi l'argument + 1
  * Le return est implicite
- * 
- * contrainte: 
+ *
+ * contrainte:
  *   - le mot clée "function" est interdis
  *   - le mot clée "return" est interdis
  */
 
-let arrow2 = () => {}
+let arrow2 = (a) => a + 1;
 
-module.exports = {arrow1, arrow2};
+console.log(arrow1(3));
+
+console.log(arrow2(5));
+
+module.exports = { arrow1, arrow2 };
