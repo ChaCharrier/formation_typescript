1d30a0cd9c4b257ad6eb37cc8531d0230e757705
Charlotte Charrier <charlotte.charrier@maif.fr>
2026-01-20 14:42:57 +0100

    correction exercice 13

--- 13_promise.ts
+++ 13_promise.ts
@@ -4,7 +4,7 @@
  * Créer une fonction `fetchUsername(userId)` qui :
  * - Prend un `userId` de type number
  * - Retourne une `Promise<string>` contenant le nom d'utilisateur
- * - Utilise l'API JSONPlaceholder : https://jsonplaceholder.typicode.com/users/{userId}
+ * - Utilise l'API JSONPlaceholder : {userId}
  *
  * L'API retourne un objet utilisateur avec une propriété `username`.
  *
@@ -17,10 +17,34 @@
 
 // TODO: Implémenter fetchUsername
 
+type UserData = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
 export async function fetchUsername(userId: number): Promise<string> {
   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
 
-  const data = await res.json();
+  const data = (await res.json()) as UserData;
   console.log(data.username);
   return data.username;
 }
